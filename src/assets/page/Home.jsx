c63957b5cd4fbfe37c8a23ac30260986e1aaeb7b
Maria Munoz <maria.munoz.dev@gmail.com>
2023-08-23 16:01:54 +0200

    offer selected done

--- src/assets/page/Home.jsx
+++ src/assets/page/Home.jsx
@@ -1,17 +1,19 @@
 import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 import axios from "axios";
 import Hero from "../../components/Hero";
 
 const Home = () => {
-
   const [data, setData] = useState({});
   const [isLoading, setIsLoading] = useState(true);
+
+  // const [offerSelected, setOfferSelected] = useState([]);
 
   const fetchData = async () => {
     const response = await axios.get(
       "https://lereacteur-vinted-api.herokuapp.com/offers"
     );
-     //console.log(response.data);
+    //console.log(response.data);
     setData(response.data);
     setIsLoading(false);
   };
@@ -20,9 +22,51 @@ const Home = () => {
     fetchData();
   }, []);
 
-  return isLoading ? <span>En cours de chargement... </span> : (
+  // const handleOffer = (offer) => {
+  //   //console.log(offer)
+  //   const offersCopy = [...offerSelected];
+  //   const offerPresent = offersCopy.find((elem) => elem.id === offer.id);
+  //   if (offerPresent) {
+  //     offerPresent.quantity++;
+  //   } else {
+  //     const obj = { ...offer, quantity: 1 };
+  //     offersCopy.push(obj);
+  //   }
+  //   setOfferSelected(offersCopy);
+
+  // //console.log(offers)
+  // };
+
+  return isLoading ? (
+    <span>En cours de chargement... </span>
+  ) : (
     <>
-    <Hero/>
+      <Hero />
+      <main className="container">
+        {data.offers.map((offer) => {
+          // console.log(offer)
+          return (
+            <div key={offer._id}>
+              <Link
+                to={`/offer/${offer._id}`}
+               >
+                <article>
+                  <div>
+                    <p>{offer.owner.account.username}</p>
+                  </div>
+                  <div>
+                    <img src={offer.product_image.url} alt="" />
+                  </div>
+                  <div>
+                    <p>{offer.product_price}€</p>
+                    <p>{offer.product_name}</p>
+                  </div>
+                </article>
+              </Link>
+            </div>
+          );
+        })}
+      </main>
     </>
   );
 };
--- src/assets/page/Offer.jsx
+++ src/assets/page/Offer.jsx
@@ -1,14 +1,51 @@
 import { useParams } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const Offer = () => {
-  // useParams permet de récupérer les params présent dans l'url de la page
+  const [data, setData] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
   const { id } = useParams();
-  console.log(id);
-  return (
-    <div>
-      <h1>Je suis sur la page Offer</h1>
-      <p>L'id de cette offre est : {id}</p>
+
+  useEffect(() => {
+    try {
+      const fetchData = async () => {
+        const response = await axios.get(
+          `https://lereacteur-vinted-api.herokuapp.com/offer/${id}`
+        );
+       // console.log(response.data);
+        setData(response.data);
+        setIsLoading(false);
+        console.log(data);
+      };
+      fetchData();
+    } catch (err) {
+      console.log(err);
+    }
+  }, []);
+
+  
+
+  return isLoading ? (
+    <span>No data... </span>
+  ) : (
+    <div className="offre">
+    
+      <article>
+                  <div>
+                    <img src={data.product_image.url} alt="" />
+                  </div>
+                <div>
+                    <p>{data.owner.account.username}</p>
+                  </div>
+                  <div>
+                    <p>{data.product_price}€</p>
+                    <p>{data.product_name}</p>
+                  </div>
+              </article>
+   
     </div>
+      
   );
 };
 
