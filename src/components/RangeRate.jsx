9024e4c672971947695bc6df35fa51d4342dc3aa
Maria Munoz <maria.munoz.dev@gmail.com>
2023-08-29 22:25:15 +0200

    props.id

--- src/components/RangeRate.jsx
+++ src/components/RangeRate.jsx
@@ -24,7 +24,7 @@ const RangeRate = ({ setRangePriceOffers }) => {
               display: "flex",
               width: "50%",
             }}>
-            <div
+            <div key={props.id}
               {...props}
               style={{
                 height: "5px",
